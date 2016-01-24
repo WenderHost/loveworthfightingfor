bf7a6b9af65249bc10a6e24ffe84c16c270ad309
Michael Wender <michael@michaelwender.com>
2016-01-24 06:09:29 -0500

    Updating GA ID

--- js/site.js
+++ js/site.js
@@ -75,16 +75,14 @@ $('a.button').smoothScroll({
 	}
 
 	function googleAnalytics() {
-		/*
-		if ( window.location.hostname !== 'localhost' ) {
+		if ( window.location.hostname !== 'loveworthfightingfor.dev' ) {
 			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 				(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 				m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 			})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-			ga('create', 'UA-24389952-15', 'auto');
+			ga('create', 'UA-44982675-1', 'auto');
 			ga('send', 'pageview');
 		}
-		*/
 	}
 
 	init();
